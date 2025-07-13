4a35a870387359a61ceecbcad6f39e1392b07c9c
ppericles <pericles@ims.gr>
2025-07-13 16:35:56 +0300

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,1 +1,9 @@
 
+from PIL import Image
+from io import BytesIO
+import base64
+
+def image_to_base64(img):
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    return base64.b64encode(buffer.getvalue()).decode()
