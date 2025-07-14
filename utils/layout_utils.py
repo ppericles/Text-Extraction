5c0c1aed50cceab88dbd65a2b36251833c40ccd6
ppericles <pericles@ims.gr>
2025-07-14 23:23:17 +0300

    Update layout_utils.py

--- utils/layout_utils.py
+++ utils/layout_utils.py
@@ -1,12 +1,6 @@
-def get_form_bounding_box(form_boxes):
-    coords = []
-    for box in form_boxes.values():
-        if all(k in box for k in ("x1", "y1", "x2", "y2")):
-            coords.extend([
-                (box["x1"], box["y1"]),
-                (box["x2"], box["y2"])
-            ])
-    if not coords:
-        return None
-    xs, ys = zip(*coords)
-    return min(xs), min(ys), max(xs), max(ys)
+def get_form_bounding_box(layout):
+    x1s = [box["x1"] for box in layout.values()]
+    y1s = [box["y1"] for box in layout.values()]
+    x2s = [box["x2"] for box in layout.values()]
+    y2s = [box["y2"] for box in layout.values()]
+    return min(x1s), min(y1s), max(x2s), max(y2s)
