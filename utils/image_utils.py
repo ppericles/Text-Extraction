a0a4a5a7d3204706a5aab7e8d9634bd61be5f8d1
ppericles <pericles@ims.gr>
2025-07-21 00:18:53 +0300

    Delete utils/image_utils.py

--- utils/image_utils.py
+++
@@ -1,9 +0,0 @@
-import base64
-from io import BytesIO
-from PIL import Image
-
-def image_to_base64(pil_image):
-    buffer = BytesIO()
-    pil_image.save(buffer, format="PNG")
-    base64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return base64_image
