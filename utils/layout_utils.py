e2874035b8d074279834d5bd1c27904970fdd2cc
ppericles <pericles@ims.gr>
2025-07-21 00:18:43 +0300

    Delete utils/layout_utils.py

--- utils/layout_utils.py
+++
@@ -1,6 +0,0 @@
-def get_form_bounding_box(layout):
-    x1s = [box["x1"] for box in layout.values()]
-    y1s = [box["y1"] for box in layout.values()]
-    x2s = [box["x2"] for box in layout.values()]
-    y2s = [box["y2"] for box in layout.values()]
-    return min(x1s), min(y1s), max(x2s), max(y2s)
